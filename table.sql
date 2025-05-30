ab0e2bdd66eb0664cf53d56856909051823f2302
mitoma-09 <mitoma0092@outlook.jp>
2025-05-30 11:29:08 +0900

    table

---
+++ table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE testtable (
+    name TEXT NOT NULL,
+    exam_day INTEGER NOT NULL,
+    nLang INTEGER,
+    math INTEGER,
+    Eng INTEGER,
+    JHist INTEGER,
+    wHist INTEGER,
+    geo INTEGER,
+    phys INTEGER,
+    chem INTEGER,
+    bio INTEGER,
+    ID INTEGER PRIMARY KEY AUTOINCREMENT
+);
