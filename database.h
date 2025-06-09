43135300e66a2a67ec90606ca6e6f05b313979dc
mitoma-09 <mitoma0092@outlook.jp>
2025-06-09 10:59:46 +0900

    kyouyuuteigi

--- database.h
+++ database.h
@@ -3,6 +3,9 @@
 
 #include <sqlite3.h>
 
+// データベースファイル名の共有定義
+#define DATABASE_FILENAME "examdata.db"
+
 // データベース接続・切断
 sqlite3* connect_to_database(const char *filename);
 void close_database(sqlite3 *db);
