6d826f548b998addd67a5d7325b8f146c429f9a8
mitoma-09 <mitoma0092@outlook.jp>
2025-06-09 13:44:37 +0900

    マクロ化

--- database.h
+++ database.h
@@ -6,6 +6,23 @@
 // データベースファイル名の共有定義
 #define DATABASE_FILENAME "examdata.db"
 
+// テーブル作成用SQL文のマクロ化
+#define CREATE_TABLE_SQL \
+"CREATE TABLE IF NOT EXISTS testtable (" \
+"name TEXT NOT NULL, " \
+"exam_day INTEGER NOT NULL, " \
+"nLang INTEGER, " \
+"math INTEGER, " \
+"Eng INTEGER, " \
+"JHist INTEGER, " \
+"wHist INTEGER, " \
+"geo INTEGER, " \
+"phys INTEGER, " \
+"chem INTEGER, " \
+"bio INTEGER, " \
+"ID INTEGER PRIMARY KEY AUTOINCREMENT" \
+");"
+
 // データベース接続・切断
 sqlite3* connect_to_database(const char *filename);
 void close_database(sqlite3 *db);
